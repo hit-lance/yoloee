f005f0541f09cf1351c46386bd779215cad3e422
Jingfeng Li <jfli0611@gmail.com>
2023-05-06 12:44:49 +0800

    compress

--- compress.py
+++ compress.py
@@ -9,10 +9,12 @@ with open('frequency.pkl', 'rb') as f:
     frequency = pickle.load(f)
 codec = HuffmanCodec.from_frequencies(frequency)
 
-def linear_quantize(x):
+
+def linear_quantize(x, bits=8):
+    n = (2 << (bits - 1)) - 1
     # Compute the scale factor and zero point
     x_min, x_max = np.min(x), np.max(x)
-    scale = (x_max - x_min) / 255
+    scale = (x_max - x_min) / n
 
     # Quantize the input data
     x_q = np.round(x / scale + x_min).astype(np.uint8)
@@ -36,24 +38,25 @@ def psnr(a, b):
 def compress(x, alg='lz4'):
     x = x.reshape(-1)
     x_q, x_max, x_min = linear_quantize(x)
-    if alg=='lz4':
+    if alg == 'lz4':
         x_c = bitshuffle.compress_lz4(x_q)
-    elif alg=='huffman':
+    elif alg == 'huffman':
         x_c = codec.encode(x_q.astype('str').tolist())
     return x_c, x_max, x_min
 
 
 def uncompress(x_c, x_max, x_min, x_shape, alg='lz4'):
-    if alg=='lz4':
+    if alg == 'lz4':
         x = bitshuffle.decompress_lz4(x_c, (math.prod(x_shape), ),
-                                  np.dtype('uint8'))
-    elif alg=='huffman':
+                                      np.dtype('uint8'))
+    elif alg == 'huffman':
         x = codec.decode_streaming(x_c)
         x = np.array(list(x)).astype(np.uint8)
 
     x = linear_dequantize(x, x_max, x_min)
     x = x.reshape(x_shape)
     return x
+
 
 def benchmark():
     n = 16551
@@ -92,12 +95,18 @@ def benchmark():
         print(sum(ratio[i]) / len(ratio[i]))
         print('\n')
 
+
 if __name__ == "__main__":
     with open("inters/test/1/0.npy", 'rb') as f:
         a = np.load(f)
-    
-    a_c, a_max, a_min = compress(a, alg='huffman')
-    a_uncompressed = uncompress(a_c, a_max, a_min, a.shape, alg='huffman')
+
+    alg='huffman'
+
+    a_c, a_max, a_min = compress(a, alg=alg)
+    a_uncompressed = uncompress(a_c, a_max, a_min, a.shape, alg=alg)
     print(np.allclose(a, a_uncompressed, atol=0.01))
-    # print("compression ratio: {}".format(a.nbytes / a_c.nbytes))
-    print("compression ratio: {}".format(a.nbytes / len(a_c)))
+    
+    if  alg=='lz4':
+        print("compression ratio: {}".format(a.nbytes / a_c.nbytes))
+    elif  alg=='huffman':
+        print("compression ratio: {}".format(a.nbytes / len(a_c)))
